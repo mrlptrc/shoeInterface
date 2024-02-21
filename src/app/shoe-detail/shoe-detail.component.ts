c137d8fdcc7cec367d12d9f2d51750a586289aa4
mrlptrc <murilopatricio@gmail.com>
2024-02-21 19:05:12 -0300

    shoe detail now has a card and cancel, update buttons and sonar fixes

--- src/app/shoe-detail/shoe-detail.component.ts
+++ src/app/shoe-detail/shoe-detail.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ShoeDTO } from '../shoe-dto.model';
-import { ShoeService } from '../shoe.service'; // Importe o serviço
+import { ShoeService } from '../shoe.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PopupService } from '../popup.service';
 
 @Component({
   selector: 'app-shoe-detail',
@@ -9,13 +11,23 @@ import { ShoeService } from '../shoe.service'; // Importe o serviço
   styleUrls: ['./shoe-detail.component.css']
 })
 export class ShoeDetailComponent implements OnInit {
-  shoe!: ShoeDTO;
+  shoe: ShoeDTO = { id: '', brand: '', model: '', price: 0 };
+  updateShoe: FormGroup;
 
   constructor(
+    private fb: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
-    private shoeService: ShoeService
-  ) {}
+    private shoeService: ShoeService,
+    private popupService: PopupService
+  ) {
+    this.updateShoe = this.fb.group({
+      updatedBrand: ['', Validators.required],
+      updatedModel: ['', Validators.required],
+      updatedPrice: ['', Validators.min(0)],
+    });
+
+  }
 
   ngOnInit() {      
     this.route.params.subscribe(params => {
@@ -26,4 +38,35 @@ export class ShoeDetailComponent implements OnInit {
       });
     });
   }
+
+  cancelButton(){
+    this.router.navigate(['/shoes']);
+  }
+
+  goUpdate(){    
+    if(this.updateShoe.valid){
+      const updatedBrand = this.updateShoe.get('updatedBrand')?.value;
+      const updatedModel = this.updateShoe.get('updatedModel')?.value;
+      const updatedPrice = this.updateShoe.get('updatedPrice')?.value;
+
+      const updatedShoeData = {
+        brand: updatedBrand,
+        model: updatedModel,
+        price: updatedPrice
+      };
+
+      if (this.shoe.id) {
+        this.shoeService.updateShoe(this.shoe.id, updatedShoeData).subscribe({
+          next: (response) => {
+            this.popupService.openPopup('Shoe updated successfully!', response);
+            console.log("passoaq");
+          },
+          error: (error) => {
+            this.popupService.openPopup('An error ocurred updating a shoe.', error);
+          }
+        });
+      }
+  }
 }
+}
+
